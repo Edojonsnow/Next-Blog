a122bcda7331970ae4b0e1176050bf4265e31dcf
Alexander Oronsaye <osahenrumeh13@gmail.com>
2024-02-28 16:09:56 +0100

    addes hamburger menu and made page responsive

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
+import styles from "./auth.module.css";
 
 const Auth = () => {
   const status = "authenticated";
+  const [open, setIsOpen] = useState(false);
   return (
     <>
-      {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+      {status === "authenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write"> Write</Link>
+          <Link href="/write" className={styles.link}>
+            {" "}
+            Write
+          </Link>
           <span>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setIsOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.overlay}>
+          <Link href="/"> Homepage</Link>
+          <Link href="/"> Contact</Link>
+          <Link href="/"> About</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write"> Write</Link>
+              <span>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
