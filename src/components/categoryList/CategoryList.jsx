c3172bb52f82ee05b9ba8c4d776eaaa6890fe77d
Alexander Oronsaye <osahenrumeh13@gmail.com>
2024-03-01 13:53:05 +0100

    Added categories bar

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,8 +1,64 @@
 import React from "react";
 import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}> Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=personal"
+          className={`${styles.category} ${styles.personal}`}
+        >
+          <Image
+            src="/style.png"
+            width={30}
+            height={30}
+            className={styles.image}
+          />
+          Personal
+        </Link>
+        <Link
+          href="/blog?cat=tech"
+          className={`${styles.category} ${styles.tech}`}
+        >
+          <Image
+            src="/culture.png"
+            width={30}
+            height={30}
+            className={styles.image}
+          />
+          Tech
+        </Link>
+        <Link
+          href="/blog?cat=society"
+          className={`${styles.category} ${styles.society}`}
+        >
+          <Image
+            src="/travel.png"
+            width={30}
+            height={30}
+            className={styles.image}
+          />
+          Society
+        </Link>
+        <Link
+          href="/blog?cat=society"
+          className={`${styles.category} ${styles.society}`}
+        >
+          <Image
+            src="/travel.png"
+            width={30}
+            height={30}
+            className={styles.image}
+          />
+          <span className={styles.text}>Society</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
